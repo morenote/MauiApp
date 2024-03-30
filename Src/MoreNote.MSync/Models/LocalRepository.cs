44ce48f0a06483f527ee3452e56c3a7f2d3a425b
hyfree-msi <wangxianqiangcn@foxmail.com>
2024-03-30 22:24:31 +0800

    添加项目文件。

---
+++ Src/MoreNote.MSync/Models/LocalRepository.cs
@@ -0,0 +1,44 @@
+﻿using MoreNote.MSync.Services.FileSystem;
+using MoreNote.MSync.Services.FileSystem.IMPL;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.MSync.Models
+{
+    public class LocalRepository
+    {
+        /// <summary>
+        /// 基路径，可以认为是仓库文件夹的路径
+        /// </summary>
+        public string BasePath { get; set; }
+        public VirtualFileSystem fileSystemServices=new LocalFileSystem();
+        public string RepositoryConfigFile { get; set; }="config";
+
+        public string GetConfigFilePath()
+        {
+            return BasePath + RepositoryConfigFile;
+        }
+        public static LocalRepository open(string path)
+        {
+            return null;
+        }
+        /// <summary>
+        /// 初始化仓库
+        /// </summary>
+        public void Init()
+        {
+            //首先根据配置文件判断是是否是空的
+            if (fileSystemServices.File_Exists(this.GetConfigFilePath()))
+            {
+                return;
+            }
+
+
+        }
+
+    }
+}
---
+++ Src/MoreNoteMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoreNoteMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
